26c7b7380ee5fc5f55b84159d809557494a65e0b
Chetan Khajuria <chetankhajuria1493@gmail.com>
2026-04-16 13:14:19 +0530

    day1_.NET

---
+++ MyApp/Program.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+class Hello{
+ 
+  public static void Main(){
+    
+    Console.WriteLine("Enter the Number to print=");
+    int a = Convert.ToInt32(Console.ReadLine());
+    Console.WriteLine($"The Number that you entered is {a}");
+  }
+}
