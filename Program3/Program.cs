b0a15f0b3a56bfd602ed80e71b6d2cb44f7b4849
Chetan Khajuria <chetankhajuria1493@gmail.com>
2026-04-19 23:34:45 +0530

    oops program added

---
+++ Program3/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+// Base class
+class Animal
+{
+    public virtual void Sound()
+    {
+        Console.WriteLine("Animal makes a sound");
+    }
+}
+
+// Derived class
+class Dog : Animal
+{
+    public override void Sound()
+    {
+        Console.WriteLine("Dog barks");
+    }
+}
+
+// Another derived class
+class Cat : Animal
+{
+    public override void Sound()
+    {
+        Console.WriteLine("Cat meows");
+    }
+}
+
+class Program
+{
+    static void Main()
+    {
+        Animal a;
+
+        a = new Dog();
+        a.Sound();   // Dog barks
+
+        a = new Cat();
+        a.Sound();   // Cat meows
+    }
+}
