dcd4bcdbb750f628735853a0d403563175899a35
Chetan Khajuria <chetankhajuria1493@gmail.com>
2026-04-21 10:59:18 +0530

    multicast delegate code

--- Program8/Program.cs
+++ Program8/Program.cs
@@ -1,1 +1,26 @@
-﻿Console.WriteLine("Hello, World!");
+﻿using System;
+
+class Program
+{
+    delegate void MyDelegate();
+
+    static void Method1()
+    {
+        Console.WriteLine("Method 1 called");
+    }
+
+    static void Method2()
+    {
+        Console.WriteLine("Method 2 called");
+    }
+
+    static void Main()
+    {
+        MyDelegate d;
+
+        d = Method1;   // first method
+        d += Method2;  // add second method
+
+        d();  // calls both methods
+    }
+}
