1227d9cb7d2088595c605d684ebc8682a6d050b0
Chetan Khajuria <chetankhajuria1493@gmail.com>
2026-04-23 16:46:21 +0530

    Generic Code added

---
+++ Program12/Program.cs
@@ -0,0 +1,38 @@
+﻿
+using System;
+
+class Box<T>
+{
+    private T data;
+
+    public void SetValue(T value)
+    {
+        data = value;
+    }
+
+    public T GetValue()
+    {
+        return data;
+    }
+
+    public void Display()
+    {
+        Console.WriteLine("Value: " + data);
+    }
+}
+
+class Program
+{
+    static void Main()
+    {
+       
+        Box<int> intBox = new Box<int>();
+        intBox.SetValue(100);
+        intBox.Display();
+
+       
+        Box<string> strBox = new Box<string>();
+        strBox.SetValue("Hello Chetan");
+        strBox.Display();
+    }
+}
