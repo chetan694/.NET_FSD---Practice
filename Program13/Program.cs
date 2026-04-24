7ab0f602458666e5c49919534827c65781b062dd
Chetan Khajuria <chetankhajuria1493@gmail.com>
2026-04-24 21:08:45 +0530

    BubbleSort code updated

--- Program13/Program.cs
+++ Program13/Program.cs
@@ -1,2 +1,35 @@
-﻿
-Console.WriteLine("Hello Chetan");
+﻿using System;
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Enter number of elements: ");
+        int n = int.Parse(Console.ReadLine());
+
+        int[] arr = new int[n];
+
+        for (int i = 0; i < n; i++)
+        {
+            Console.Write("Enter element: ");
+            arr[i] = int.Parse(Console.ReadLine());
+        }
+
+        for (int i = 0; i < n - 1; i++)
+        {
+            for (int j = 0; j < n - i - 1; j++)
+            {
+                if (arr[j] > arr[j + 1])
+                {
+                    int temp = arr[j];
+                    arr[j] = arr[j + 1];
+                    arr[j + 1] = temp;
+                }
+            }
+        }
+
+        foreach (int num in arr)
+        {
+            Console.Write(num + " ");
+        }
+    }
+}
