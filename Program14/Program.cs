7fd86019be20ff1a8fbaa1ad1e4ee0b91eb36318
Chetan Khajuria <chetankhajuria1493@gmail.com>
2026-04-27 09:24:17 +0530

    SelectionSort code updated

---
+++ Program14/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Enter number of elements: ");
+        int n = int.Parse(Console.ReadLine());
+
+        int[] arr = new int[n];
+
+        for (int i = 0; i < n; i++)
+        {
+            Console.Write("Enter element: ");
+            arr[i] = int.Parse(Console.ReadLine());
+        }
+
+        for (int i = 0; i < n - 1; i++)
+        {
+            int minIndex = i;
+
+            for (int j = i + 1; j < n; j++)
+            {
+                if (arr[j] < arr[minIndex])
+                {
+                    minIndex = j;
+                }
+            }
+
+            int temp = arr[i];
+            arr[i] = arr[minIndex];
+            arr[minIndex] = temp;
+        }
+
+        foreach (int num in arr)
+        {
+            Console.Write(num + " ");
+        }
+    }
+}
